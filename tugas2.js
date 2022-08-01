1df1f34ec46e44f82cc731c58509b936fa4c18a3
Muhamad Allya Agung Syafira <102126267+mallyaagung@users.noreply.github.com>
2022-08-01 17:10:24 +0800

    revisi

--- tugas2.js
+++ tugas2.js
@@ -1,29 +1,24 @@
-let bind = 10
-let bing = 10
-let mat = 10
-let ipa = 10
-let grade
+let bind = 100
+let bing = 100
+let mat = 100
+let ipa = 100
 
 let ratarata = () => {
-    const re = /^[0-9]+$/;
-    if (re.test(bind && bing && mat && ipa)) {
-        let nilai = bind + bing + mat + ipa;
-        let hasil = parseInt(nilai / 4);
-
-        if (hasil >= 90 && hasil <= 100) {
-            grade = "A";
-        } if (hasil >= 80 && hasil <= 89) {
-            grade = "B";
-        } if (hasil >= 70 && hasil <= 79) {
-            grade = "C";
-        } if (hasil >= 60 && hasil <= 69) {
-            grade = "D";
-        } else {
-            grade = "E";
-        }
-        console.log(`Rata-rata = ${hasil}\nGrade = ${grade}`)
-    } else {
-        console.log(`Masukka data yang sesuai`)
+    let nilai = bind + bing + mat + ipa;
+    let hasil = parseInt(nilai / 4);
+    try {
+        if (bind == "" || bing == "" || mat == "" || ipa == "") throw `Data tidak boleh kosong`
+        if (isNaN(bind) || isNaN(bing) || isNaN(mat) || isNaN(ipa)) throw `Data bukan angka`
+        bind, bing, mat, ipa = Number(bind, bing, mat, ipa);
+        if (nilai < 0 || nilai > 400) throw `Data yang dimasukkan tidak valid`
+        if (hasil >= 90 && hasil <= 100) throw `Rata-rata = ${hasil}\nGrade = A`
+        if (hasil >= 80 && hasil <= 89) throw `Rata-rata = ${hasil}\nGrade = B`
+        if (hasil >= 70 && hasil <= 79) throw `Rata-rata = ${hasil}\nGrade = C`
+        if (hasil >= 60 && hasil <= 69) throw `Rata-rata = ${hasil}\nGrade = D`
+        if (hasil >= 0 && hasil <= 59) throw `Rata-rata = ${hasil}\nGrade = E`
+    } catch (err) {
+        console.log(err)
     }
 }
-ratarata();
+
+ratarata()
